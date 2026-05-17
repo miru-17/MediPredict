b93740d3d7f449feca39eb6b5960e445038d3812
miru-17 <mirudhulakathirvel@gmail.com>
2026-05-17 22:40:30 +0530

    Add files via upload

--- database.py
+++ database.py
@@ -2,6 +2,16 @@ from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["healthanalyserdb"]
-users_collection = db["users"]
 
-print("✅ Connected to healthanalyserdb → users collection")
+predictions_collection = db["predictions"]
+predictions_collection.insert_one({
+    "test": "connection successful"
+})
+
+print("✅ Database + collection confirmed")
+predictions_collection.insert_one({
+    "test": "connection successful"
+})
+
+print("✅ Database + collection confirmed")
+
--- predictor.py
+++ predictor.py
@@ -7,35 +7,19 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from predict_disease import predict_disease
 
 
-def run_prediction(symptoms):
-
+def run_prediction(user_email, symptoms):
     prediction = predict_disease(symptoms)
 
-    # prediction currently returns:
-    # ('Hypertension ', 26.0, 'Low', 'Low risk but maintain precautions.')
-
-    disease = prediction[0]
-    confidence = prediction[1]
-    risk = prediction[2]
-    advice = prediction[3]
-
     return {
-
-        "disease": disease,
-        "confidence": confidence,
-        "risk": risk,
-        "advice": advice,
-        "symptoms": symptoms
-
+        "email": user_email,
+        "symptoms": symptoms,
+        "prediction": prediction
     }
 
 
 if __name__ == "__main__":
-
     result = run_prediction(
-
-        symptoms=["fatigue","headache","cough","chest_pain"]
-
+        user_email="test@gmail.com",
+        symptoms=["fatigue", "headache"]
     )
-
     print(result)
